0b3e527f24214de2adbdc9fb6ca93f3aaccd3742
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2024-01-18 20:46:32 +0200

    objects created

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import MainProduct from "./components/MainProduct.vue";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const products = [
+  {
+    icon: "bi bi-twitch text-purple-700",
+    name: "Twitch Posts",
+    newValue: "500.00",
+    unit: "$",
+    value: "+40.5% ",
+    valueColor: "text-green-500",
+    moving: "more impressions",
+    info: "0.5% MRR",
+  },
+  {
+    icon: "bi bi-twitter text-blue-500",
+    name: "Twitter Folloers",
+    newValue: "807",
+    unit: "k",
+    value: "+17.62% ",
+    valueColor: "text-green-500",
+    moving: "Followers Growth",
+    info: "5% New trials",
+  },
+  {
+    icon: "bi bi-spotify text-green-500",
+    name: "Spotify Listeners",
+    newValue: "1,073",
+    unit: "",
+    value: "+10.45% ",
+    valueColor: "text-red-500",
+    moving: " Less comments than usual",
+    info: "40% Impressions",
+  },
+  {
+    icon: "bi bi-pinterest text-red-500",
+    name: "Pinterest Posts",
+    newValue: "97",
+    unit: "",
+    value: "+26.1% ",
+    valueColor: "text-green-500",
+    moving: "More posts",
+    info: "10% Spend",
+  },
+  {
+    icon: "bi bi-github text-purple-500",
+    name: "Github Contributes",
+    newValue: "4,109",
+    unit: "",
+    value: "+32.8% ",
+    valueColor: "text-red-500",
+    moving: " Less contributions",
+    info: "40% Dispute",
+  },
+  {
+    icon: "bi bi-youtube text-red-500",
+    name: "Youtube Subscribers",
+    newValue: "354",
+    unit: "",
+    value: "+29.45% ",
+    valueColor: "text-green-500",
+    moving: "Subscribers Growth",
+    info: "40% Subscribers",
+  },
+];
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-row ms-5">
+    <MainProduct v-for="product in products" :details="product" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 </style>
---
+++ src/components/MainProduct.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+  details: Object,
+});
+</script>
+
+<template>
+  <div
+    class="ps-5 pe-5 py-3 m-3 bg-white border border-gray-200 rounded-lg shadow"
+  >
+    <div class="flex justify-between">
+      <div class="flex items-center pe-14">
+        <i :class="details.icon" class="pe-5 text-2xl"></i>
+        <p class="text-xl font-bold text-gray-500">{{ details.name }}</p>
+      </div>
+      <i class="bi bi-grid text-blue-600"></i>
+    </div>
+
+    <h5 class="my-5 text-4xl font-bold tracking-tight text-black">
+      {{ details.newValue }} {{ details.unit }}
+    </h5>
+    <div class="flex">
+      <p :class="details.valueColor" class="mb-3 me-3 font-bold">
+        {{ details.value }}
+      </p>
+      <p class="mb-3 font-normal">{{ details.moving }}</p>
+    </div>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+      {{ details.info }}
+    </p>
+    <p></p>
+  </div>
+</template>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
